f0f951246f73f52c3c3a8059c77d8684dd936d0c
masoudnia68-star <masoudnia68@gmail.com>
2026-06-19 20:24:32 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Service Worker - شهربازی اعداد اعشاری
+const CACHE_NAME = 'decimal-game-v1';
+const FILES_TO_CACHE = [
+  './',
+  './decimal_game.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
